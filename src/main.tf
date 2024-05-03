905b5745ddeac53eb96f7dea510a483a4e8313ff
PRADHAN <sayantan.pradhan@capgemini.com>
2024-05-03 11:22:22 +0530

    .

--- src/main.tf
+++ src/main.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 4.16"
     }
   }
+
+  required_version = ">= 1.2.0"
+
+  backend "s3" {
+       bucket = "sayantp-backend"
+       key    = "sayantp-backend.tfstate"
+       region = "us-east-1"
+   }
 }
 
 provider "aws" {
