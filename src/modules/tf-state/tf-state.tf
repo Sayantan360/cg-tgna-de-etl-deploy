29e79e5b04165e0bee16f0ec08c01a055161d217
PRADHAN <sayantan.pradhan@capgemini.com>
2024-05-03 16:01:19 +0530

    .

--- src/modules/tf-state/tf-state.tf
+++ src/modules/tf-state/tf-state.tf
@@ -2,3 +2,10 @@ resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
   force_destroy = true
 }
+
+resource "aws_s3_object" "file" {
+  bucket = "aws_s3_bucket.bucket.id"
+  key    = "glue_script.py"
+  source = "src/glue_script.py"
+
+}
